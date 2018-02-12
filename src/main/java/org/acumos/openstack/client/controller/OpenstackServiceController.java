e06a92ef62684a83e8807d5a8f4221b73314d663
Santosh Kumar18 <SK00472486@TechMahindra.com>
2018-02-12 21:14:09 +0530

    cd-1816 : Add junit and license in openstack
    Change-Id: Ieda965c488e55b621cb4c4260e27990c01b00a89
    Signed-off-by: Santosh Kumar18 <SK00472486@TechMahindra.com>

--- src/main/java/org/acumos/openstack/client/controller/OpenstackServiceController.java
+++ src/main/java/org/acumos/openstack/client/controller/OpenstackServiceController.java
@@ -1,3 +1,22 @@
+/*-
+ * ===============LICENSE_START=======================================================
+ * Acumos
+ * ===================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+ * ===================================================================================
+ * This Acumos software file is distributed by AT&T and Tech Mahindra
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * This file is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ===============LICENSE_END=========================================================
+ */
 package org.acumos.openstack.client.controller;
 
 
@@ -16,6 +35,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.acumos.openstack.client.api.APINames;
+import org.acumos.openstack.client.service.impl.OpenstackSimpleSolution;
 import org.acumos.openstack.client.transport.OpenstackDeployBean;
 import org.json.JSONObject;
 
@@ -88,9 +108,9 @@ public class OpenstackServiceController extends AbstractController {
 		
 		 uidNumStr=uidNumber.toString();
 		 jsonOutput.put("Status", uidNumStr);
-		// OpenstackSimpleSolution opSingleSolution=new OpenstackSimpleSolution(os,flavourName,securityGropName,auth);
-		 //Thread t = new Thread(opSingleSolution);
-        // t.start();
+		 OpenstackSimpleSolution opSingleSolution=new OpenstackSimpleSolution(os,flavourName,securityGropName,auth);
+		 Thread t = new Thread(opSingleSolution);
+         t.start();
 		 
 		 
 		}catch(Exception e){
